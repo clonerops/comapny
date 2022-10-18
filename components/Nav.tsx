655b69dcdfb53f50bee3f0d2b60277143fc2ff87
clonerops <114849678+clonerops@users.noreply.github.com>
2022-10-18 09:52:42 +0330

    nav responsive not completed

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Bars2Icon } from '@heroicons/react/24/solid'
 
 const Nav = () => {
     return (
         <div className='nav'>
+            <input type='checkbox' className='nav__menu' id='menu' />
+            <label htmlFor='menu' className='nav__label'>
+                <span></span>
+                <span></span>
+            </label>
+
+            <div className='nav__responsive'>
+                <ul className='nav__lists'>
+                    <li className='nav__list'><a className='nav__link'>HOME</a></li>
+                    <li className='nav__list'><a className='nav__link'>ABOUT</a></li>
+                    <li className='nav__list'><a className='nav__link'>CLIENT</a></li>
+                    <li className='nav__list'><a className='nav__link'>SOULTION</a></li>
+                    <li className='nav__list'><a className='nav__link'>CONTACT</a></li>
+                </ul>
+
+            </div>
+
             <ul className='nav__lists'>
                 <li className='nav__list'><a className='nav__link'>HOME</a></li>
                 <li className='nav__list'><a className='nav__link'>ABOUT</a></li>
